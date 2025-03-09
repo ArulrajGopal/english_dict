dd18df353f17fb71d628974ae6790bf790890d87
Arulraj Gopal <arulrajgopal@outlook.com>
2025-03-09 15:02:28 +0530

    update

--- workflow/YTChannelConfig.py
+++ workflow/YTChannelConfig.py
@@ -2,14 +2,16 @@ channel_id_dict= {
 "EQSIS":"UCKTWY-rVwUqCxrVmPOlJyjA"
 ,"OPTIONWITHAK":"UCslsTpdkrVnZSwxawhJN6Ag"
 ,"TradeAchievers":"UCzk4zJEoZMnjvpoN0HlKjHQ"
-,"MoneyPechu" : "UC7fQFl37yAOaPaoxQm-TqSA"
-,"CauveryBusiness" : "UCa1FSXPOxb0x8lZTYCbQJ5g"
-,"Muthaleetukalam":"UCahsYnjbRheSL7uv6jWbJ4w"
+,"financeboosan":"UCYoh05-njQGGmQ20V-3DvnQ"
 ,"CapitalZone":"UCxoM_zP4Cr9LpIEn3TEZhvg"
-,"PRSundar":"UCS2NdYUmv_PUyyKeDAo5zYA"
 ,"TheMadrasTrader":"UCqxH7wzv-sMrCnjP3lM3Nmw"
 ,"SagaContraTrading":"UChaRiZ3h9JlLCeO2pdWkxMw"
-,"TiruppurBullsShares":"UCqhL6vNCwYLC9_jePXOIvBg"
+,"ddnifty":"UCYRB8kDbaW6a1Gufr_qwVTA"
+# ,"TiruppurBullsShares":"UCqhL6vNCwYLC9_jePXOIvBg"
+# ,"PRSundar":"UCS2NdYUmv_PUyyKeDAo5zYA"
+# ,"Muthaleetukalam":"UCahsYnjbRheSL7uv6jWbJ4w"
+# ,"CauveryBusiness" : "UCa1FSXPOxb0x8lZTYCbQJ5g"
+# ,"MoneyPechu" : "UC7fQFl37yAOaPaoxQm-TqSA"
 }
 
 
