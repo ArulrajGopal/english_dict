5c633caa9b04d786e5f42c07282239c6a5932e59
Arulraj Gopal <arulrajgopal@outlook.com>
2025-03-09 16:36:30 +0530

    update

--- workflow/secrets_config.py
+++ workflow/secrets_config.py
@@ -2,6 +2,3 @@ import os
 youtube_api_key = os.environ.get("youtube_api_key")
 aws_access_key_id=os.environ.get("aws_access_key_id")
 aws_secret_key= os.environ.get("aws_secret_access_key")
-
-print(youtube_api_key)
-
